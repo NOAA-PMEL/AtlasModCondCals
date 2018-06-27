ddc99fa5357aeaa917d337fb9be09a67d2692db4
Daryn A. White <daryn.white@noaa.gov>
2018-06-26 17:31:36 -0700

    Continuing dev. Ready for outside dev

--- A2CondCals.py
+++ A2CondCals.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 ## Building an automation script for Atlas Module CondCals
 ## Author: Daryn White; daryn.white@noaa.gov
 # Last Edit: 2018-06-25 DAW 
 import sys
+import os
 import argparse
 import re
 
 ## Parse arguments
-parser = argpase.ArgumentParser(prog="A2CondCals",description="Build files and get cell_info to begin a session of conductivity calibrations")
+parser = argparse.ArgumentParser(prog="A2CondCals",description="Build files and get cell_info to begin a session of conductivity calibrations")
 parser.add_argument('fl',metavar='file',help='AT1/bath file from SeaBird')
+args = parser.parse_args()
 
+# Find the cal part
+cal = re.compile(r".+(?=\s{14,}Drift)")
+# Find the autosal part
+sal = re.compile(r"(?=\bDrift).+")
+
+# Read the file
+with open(args.fl,'r') as f:
+  data = f.read()
+  # print(data)
+  calOut = cal.match(data)
+  print(calOut)
