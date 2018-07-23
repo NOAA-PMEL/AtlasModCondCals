dfb0ad4e388157803e80c41669f91a67ea4da2f8
Daryn A. White <daryn.white@noaa.gov>
2018-07-23 15:28:55 -0700

    Continued development. Reading date from filename.

--- A2CondCals.py
+++ A2CondCals.py
@@ -6,10 +6,11 @@ import sys
 import os
 import argparse
 import re
+import datetime
 
 ## Parse arguments
 parser = argparse.ArgumentParser(prog="A2CondCals",description="Build files and get cell_info to begin a session of conductivity calibrations")
-parser.add_argument('fl',metavar='file',help='AT1/bath file from SeaBird')
+parser.add_argument('fl',metavar='file',help='Bath file from SeaBird')
 args = parser.parse_args()
 
 # Find the cal part
@@ -17,9 +18,14 @@ cal = r".+(?=\s{14,}Drift)"
 # Find the autosal part
 sal = r"(?=\bDrift).+"
 
-# Read the file
+# Read the file & make the strings needed for new files
 with open(args.fl,'r') as f:
   fl = f.read()
   calOut = re.search(cal,fl,re.DOTALL)
   salOut = re.search(sal,fl,re.DOTALL)
 
+# print(calOut.group())
+# print(salOut.group())
+dt = datetime.datetime.strptime(args.fl[:7],'%d%b%y')
+jt = dt.strftime('%y%j')
+print(jt)
