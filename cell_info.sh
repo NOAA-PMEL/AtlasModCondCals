9a9c5359ded1551f584280b0738c60c0a2743297
Daryn White <daryn.white@noaa.gov>
2022-02-14 19:46:21 -0800

    Update cell_info.sh
    Some minor adjustments from bash linting

--- cell_info.sh
+++ cell_info.sh
@@ -5,13 +5,12 @@ if [ $# -lt 1 ]; then
    exit
 fi
 
-cell_list=$@
+cell_list=$*
 
 MYSQL="mysql -N -h yawl TAO_cal"
 
-echo;
-for cell in ${cell_list}
-do
+echo
+for cell in ${cell_list}; do
    sql="SELECT TempInfo.module_type,TempInfo.sensor_id,ModCell.cell_id \
 FROM TempInfo,ModCell \
 WHERE TempInfo.sensor_id=ModCell.module_id \
@@ -19,10 +18,10 @@ AND cell_type='C' \
 AND cell_id=${cell} \
 ORDER BY ModCell.built_dt DESC \
 LIMIT 1;"
-#    echo ${sql}
-   result=`${MYSQL} --execute="${sql}" | tr -cd '[:alnum:]\t\n'`
+   #    echo ${sql}
+   result=$(${MYSQL} --execute="${sql}" | tr -cd '[:alnum:]\t\n')
    if [ -n "${result}" ]; then
-      echo ${result}
+      echo "${result}"
    else
       echo "*** No result for cell ${cell}"
    fi
